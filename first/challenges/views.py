99e4ac1f9325bc426635719f10453b60cd0d1319
mehrnaz <mehrnazbg1381@gmail.com>
2024-01-27 13:59:21 +0330

    dynamic urls

--- first/challenges/views.py
+++ first/challenges/views.py
@@ -14,6 +14,27 @@ days = {
     'friday': 'this is friday in disctionary',
 }
 
+def days_list(request):
+
+    days_list = list(days.keys())
+    list_items = ""
+    
+
+    for day in days_list:
+        url_path = reverse('days-of-week' , args=[day])
+        list_items += f'<li> <a href="{url_path}" > {day} </a> </li> \n'
+
+
+    content = """ 
+    <ul>
+
+    </ul
+            """
+    return HttpResponse(content)
+
+
+
+
 def dynamic_days_by_number(request , day ):
     days_names = list(days.keys())
     if day > len(days_names):
@@ -27,6 +48,7 @@ def dynamic_days_by_number(request , day ):
 def dynamic_days(reqeust, day):
     day_data = days.get(day)
     if day_data is not None:
-        return HttpResponse(f'day is : {day} and data is : {day_data}')
+        response_data = f' <h1 style="color: red" >day is : {day} and data is : {day_data}</h1> '
+        return HttpResponse(response_data)
     
     return HttpResponseNotFound('day does not exists')
