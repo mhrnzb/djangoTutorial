287127b54249ea25d543697b2897ef3b8d6a5eb7
mehrnaz <mehrnazbg1381@gmail.com>
2024-01-30 18:01:00 +0330

    header.html created and used with include method

--- first/challenges/urls.py
+++ first/challenges/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('' , views.days_list),
+    path('' , views.days_list , name='days_list'),
     path('<int:day>', views.dynamic_days_by_number),
     path('<str:day>', views.dynamic_days , name='days-of-week'),
     
